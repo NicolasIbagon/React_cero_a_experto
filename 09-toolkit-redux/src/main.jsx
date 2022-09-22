e89511f14197f9cd31f3a661faf1ca8d7b7da5bc
NicolasIbagon <nicolasibagon99@javerianacali.edu.co>
2022-09-22 18:28:50 -0500

    Pokemon App Parte 1

---
+++ 09-toolkit-redux/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Provider } from 'react-redux'
+import './index.css'
+import { store } from './store/store'
+import { PokemonApp } from './PokemonApp'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store = {store}><PokemonApp /></Provider>
+    
+  </React.StrictMode>
+)
