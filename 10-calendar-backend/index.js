ef61bbba4a764877b65ede90da8a649dd9239cbe
NicolasIbagon <nicolasibagon99@javeriancali.edu.co>
2022-10-10 14:56:07 -0500

    Calendar Backend

---
+++ 10-calendar-backend/index.js
@@ -0,0 +1,21 @@
+const { response } = require('express');
+const express = require('express');
+require('dotenv').config();
+
+
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.use('/api/auth', require('./routes/auth'));
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Servidor corriendo en 4000")
+});
+
+
+
