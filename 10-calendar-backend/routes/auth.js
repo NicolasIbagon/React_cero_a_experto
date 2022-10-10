20349912b1e79ca6f0bd8987375655fab8c66def
NicolasIbagon <nicolasibagon99@javeriancali.edu.co>
2022-10-10 14:55:11 -0500

    Calendar Backend

---
+++ 10-calendar-backend/routes/auth.js
@@ -0,0 +1,20 @@
+
+
+
+
+const {Router} = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+
+    response.json({
+        ok: true
+    })
+})
+
+
+
+
+
+
+module.exports = router;
